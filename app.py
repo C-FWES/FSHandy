4fedebea71b031ecf8080b3cb3b5874293104049
Wesley Ou <69126784+C-FWES@users.noreply.github.com>
2022-04-16 16:39:15 -0400

    add respective routing

--- app.py
+++ app.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+@app.route('/convert')
+def convert():
+    return render_template('conversions.html')
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
+
+@app.route('/perf')
+def perf():
+    return render_template('perf.html')
+
+@app.route('/frequencies')
+def freq():
+    return render_template('frequencies.html')
+
 
 if __name__ == '__main__':
     app.run()
